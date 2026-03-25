db0227ba0dfc66eba6bf4b6177aeaf5a56c84996
Artur Davydov <arturdav@datasoft.local>
2026-03-25 13:44:26 +0200

    Save local changes before merge

--- Tables/dbo.TestTable.sql
+++ Tables/dbo.TestTable.sql
@@ -1,6 +1,9 @@
-﻿CREATE TABLE [dbo].[TestTable] (
+﻿-- Test update #237518 - "Checkout error: Your local changes to the following files would be overwritten by merge:"
+
+CREATE TABLE [dbo].[TestTable] (
   [Id] [int] NULL,
   [Name] [nvarchar](100) NULL,
+  [TestChange1] NVARCHAR(100) NOT NULL,
   [CreatedAt] [datetime] NULL
 )
 ON [PRIMARY]
