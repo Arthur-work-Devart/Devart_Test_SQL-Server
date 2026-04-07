dd1c270fd6160ef0ee7c7fe5aa804f87d2923558
Artur Davydov <arturdav@datasoft.local>
2026-04-07 15:23:22 +0300

    Test Commit EP

---
+++ Views/dbo.TestView.sql
@@ -0,0 +1,10 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE VIEW [dbo].[TestView]
+AS
+SELECT 1 AS Col1;
+GO
+
+DECLARE @value SQL_VARIANT = CAST('' AS varchar(1))
+EXEC sys.sp_addextendedproperty N'PK', @value, 'SCHEMA', N'dbo', 'VIEW', N'TestView', 'COLUMN', N'Col1'
+GO
