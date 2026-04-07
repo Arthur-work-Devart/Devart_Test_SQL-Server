5735f3de605134648f8448251830bc111370de9c
Artur Davydov <arturdav@datasoft.local>
2026-04-07 15:39:48 +0300

    Тест Комит 2

--- Views/dbo.TestView.sql
+++ Views/dbo.TestView.sql
@@ -2,9 +2,11 @@
 GO
 CREATE VIEW [dbo].[TestView]
 AS
-SELECT 1 AS Col1;
+SELECT 1 AS Col1,
+       2 AS Col2;
+GO
+
+EXEC sys.sp_addextendedproperty N'PK', 'changed', 'SCHEMA', N'dbo', 'VIEW', N'TestView', 'COLUMN', N'Col1'
 GO
 
 DECLARE @value SQL_VARIANT = CAST('' AS varchar(1))
-EXEC sys.sp_addextendedproperty N'PK', @value, 'SCHEMA', N'dbo', 'VIEW', N'TestView', 'COLUMN', N'Col1'
-GO
