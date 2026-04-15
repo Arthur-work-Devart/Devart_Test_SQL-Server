449563ba6e04327d333bfd49ffd1a5fd99ea17ef
Artur Davydov <arturdav@datasoft.local>
2026-04-15 17:06:08 +0300

    TestCommit3

--- Views/dbo.v_Test_NS.sql
+++ Views/dbo.v_Test_NS.sql
@@ -2,7 +2,10 @@
 GO
 CREATE   VIEW [dbo].[v_Test_NS]
 AS
-WITH XMLNAMESPACES ('urn:test' AS ns)
+WITH XMLNAMESPACES (
+    'urn:test1' AS ns1,
+    'urn:test2' AS ns2
+                    )
 SELECT
     t.Id
 FROM dbo.TestXml t;
